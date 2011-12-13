f3800a3500fe722b0d3bd29d4350d425359c3d60
Travis Dixon <the.trav@gmail.com>
2011-12-13 11:23:43 +1100

    jar up class files to produce smaller classpath for jvm arg limit

--- scripts/_CucumberSetup.groovy
+++ scripts/_CucumberSetup.groovy
@@ -9,32 +9,32 @@ includeTargets << grailsScript("RunWar")
 generateLog4jFile = true
 
 target(setup: "The description of the script goes here!") {
-    depends(classpath, checkVersion, parseArguments)
+   depends(classpath, checkVersion, parseArguments)
 
-    taskdef (name: 'run-cucumber', classname: 'cuke4duke.ant.CucumberTask')
+   taskdef (name: 'run-cucumber', classname: 'cuke4duke.ant.CucumberTask')
+   jar(destfile: "${cucumberPluginDir}/class.jar", basedir: "${grailsSettings.classesDir.path}") {
+       include name: "**/*.class"
+   }
 
-    def jrubyHome = "${cucumberPluginDir}/lib/.jruby"
-    property(name: 'jruby.home', value: jrubyHome)
-    path(id: 'jruby.classpath') {
-        fileset( dir: "${grailsSettings.grailsHome}/lib") {
-            include name: "**/*.jar"
-        }
+   def jrubyHome = "${cucumberPluginDir}/lib/.jruby"
+   property(name: 'jruby.home', value: jrubyHome)
+   path(id: 'jruby.classpath') {
+       fileset( dir: "${grailsSettings.grailsHome}/lib") {
+           include name: "**/*.jar"
+       }
 
-        fileset(dir: "${cucumberPluginDir}") {
-            include name: "**/*.jar"
-        }
-        fileset(dir: "${grailsSettings.classesDir.path}") {
-            include name: "**/*.class"
-        }
-        fileset(dir: "lib") {
-            include name: "**/*.jar"
-        }
-    }
-    property(name: 'jruby.classpath', refid: 'jruby.classpath')
-    //echo('${jruby.classpath}')
-    if (!new File(jrubyHome).isDirectory()) {
-        echo("Installing required JRuby gems...")
-        taskdef(name:"gem", classname:"cuke4duke.ant.GemTask")
-        gem args: "install cuke4duke --version 0.3.0"
-    }
+       fileset(dir: "${cucumberPluginDir}") {
+           include name: "**/*.jar"
+       }
+       fileset(dir: "lib") {
+           include name: "**/*.jar"
+       }
+   }
+   property(name: 'jruby.classpath', refid: 'jruby.classpath')
+   //echo('${jruby.classpath}')
+   if (!new File(jrubyHome).isDirectory()) {
+       echo("Installing required JRuby gems...")
+       taskdef(name:"gem", classname:"cuke4duke.ant.GemTask")
+       gem args: "install cuke4duke --version 0.3.0"
+   }
 }
